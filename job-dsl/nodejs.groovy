14bbfccb8e7b2bcec9d6366704e733614c2e8fda
Daniel Alao <oloyedealao02@gmail.com>
2022-02-24 09:40:07 -0600

    second commit

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
-job('NodeJS example') {
+job('NodeJS example mine') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/danalao/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('oloyedealao02@gmail.com')
         }
     }
     triggers {
